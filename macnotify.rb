98ed3c9c97e0c724767cef38dc8310f4d6b2ae24
Akira Ouchi <akkiesoft@marokun.net>
2018-05-10 00:46:41 +0900

    mikutterアイコンとユーザーのアイコンを表示する

--- macnotify.rb
+++ macnotify.rb
@@ -5,12 +5,21 @@ Plugin.create(:macnotify) do
     text = text.to_s.encode(Encoding::UTF_8) if text.is_a? Message
     
     u = "mikumiku"
-    u = "@#{user[:idname]} (#{user[:name]})" if user
-
+    icon = nil
+    if user
+      u = "@#{user[:idname]} (#{user[:name]})"
+      icon = user.icon.perma_link.to_s
+    end
     u = u.encode(Encoding::UTF_8)
 
     if text.valid_encoding? && u.valid_encoding?
-      TerminalNotifier.notify(text, :title=>u, :sender=>'org.macosforge.xquartz.X11')
+      TerminalNotifier.notify(
+        text,
+        :title=>u,
+        :sender=>'org.macosforge.xquartz.X11',
+        :appIcon=> Skin.photo('icon.png').path,
+        :contentImage=> icon
+      )
     end
     stop.call
   end
