a57cdb4b328b9c5a4beea769b2f6072fc359e2ea
Akira Ouchi <akkiesoft@marokun.net>
2018-05-10 00:45:50 +0900

    text.to_showのかわりにtext.to_sを使う

--- macnotify.rb
+++ macnotify.rb
@@ -2,16 +2,11 @@ require "terminal-notifier"
 
 Plugin.create(:macnotify) do
   on_popup_notify do |user, text, &stop|
-    text = if text.is_a? Message
-      text.to_show
-    else
-      '' unless test.is_a? String
-    end
+    text = text.to_s.encode(Encoding::UTF_8) if text.is_a? Message
     
     u = "mikumiku"
     u = "@#{user[:idname]} (#{user[:name]})" if user
 
-    text = text.encode(Encoding::UTF_8)
     u = u.encode(Encoding::UTF_8)
 
     if text.valid_encoding? && u.valid_encoding?
